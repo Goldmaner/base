3a06c4662cc87c39b739d7676ee9cbe06198671c
Goldmaner <jeffersonluiz@prefeitura.sp.gov.br>
2025-10-17 11:38:56 -0300

    Deploy_3

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ Configurações da aplicação Flask
 import os
 
 DB_CONFIG = {
-    'host': os.environ.get('PGHOST'),
-    'port': os.environ.get('PGPORT', '5432'),
-    'database': os.environ.get('PGDATABASE'),
-    'user': os.environ.get('PGUSER'),
-    'password': os.environ.get('PGPASSWORD')
+    'host': 'shinkansen.proxy.rlwy.net',  # valor do host público
+    'port': '38157',                      # valor da porta pública
+    'database': 'railway',                # nome do banco
+    'user': 'postgres',                   # usuário
+    'password': 'sKOzVlsxAUcRIXXLynePvvHDQpXlmTVT'  # sua senha
 }
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-padrao')
